f01319c760259f0b0a051f9c22462f0a7d4640d7
sul3d <edanil5dy@gmail.com>
2023-07-17 13:47:03 -0500

    added DeckStepDefs

---
+++ src/test/java/com/MeetSky/step_definitions/DeckStepDefs.java
@@ -0,0 +1,34 @@
+package com.MeetSky.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class DeckStepDefs {
+
+    @Given("the user is on the Deck Module page")
+    public void the_user_is_on_the_deck_module_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Given("user clicks on the three lines to open navigation")
+    public void user_clicks_on_the_three_lines_to_open_navigation() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Given("user clicks on Add board")
+    public void user_clicks_on_add_board() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Given("user types a board name")
+    public void user_types_a_board_name() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("user's newly created board is displayed under All boards")
+    public void user_s_newly_created_board_is_displayed_under_all_boards() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+}
