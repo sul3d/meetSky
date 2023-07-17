ee6e02f5e5672ceedbcf750a735fc96d82bd7624
sul3d <edanil5dy@gmail.com>
2023-07-17 16:07:23 -0500

    added DeckPage

---
+++ src/test/java/com/MeetSky/pages/DeckPage.java
@@ -0,0 +1,32 @@
+package com.MeetSky.pages;
+
+
+import com.MeetSky.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
+public class DeckPage {
+
+    public DeckPage(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(xpath = "//div[contains(@class, 'close')]")
+    public WebElement closedNavigationBtn;
+
+    @FindBy(xpath = "//button[@aria-controls='app-navigation-vue']")
+    public WebElement threeLineNavigationBtn;
+
+    @FindBy(xpath = "//span[normalize-space(text())='Add board']")
+    public WebElement addBoardLink;
+
+    @FindBy(xpath = "//input[@placeholder='Board name']")
+    public WebElement boardNameInput;
+
+    @FindBy(xpath = "//ul[@class='app-navigation-entry__children']//span[@title]")
+    public List<WebElement> boards;
+
+}
