558d6304e531660cf12997b77acb25d7f6bdec22
sul3d <edanil5dy@gmail.com>
2023-07-18 19:30:21 -0500

    updated deckModule, DeckPage, and DeckStepDefs

--- src/test/java/com/MeetSky/pages/DeckPage.java
+++ src/test/java/com/MeetSky/pages/DeckPage.java
@@ -26,15 +26,58 @@ public class DeckPage {
     @FindBy(xpath = "//input[@placeholder='Board name']")
     public WebElement boardNameInput;
 
-    @FindBy(xpath = "//ul[@class='app-navigation-entry__children']//span[@title]")
-    public List<WebElement> boards;
+    @FindBy(xpath = "//ul[@class='app-navigation__list']//ul//a")
+    public List<WebElement> allBoards;
 
-    @FindBy(xpath = "//button[@icon='icon-add']")
+    @FindBy(css = ".controls>.board-title>h2")
+    public WebElement selectedBoardName;
+
+    @FindBy(xpath = "//div[@id='stack-add']//button")
     public WebElement addListBtn;
 
-    @FindBy(xpath = "//div[@id='stack-add']//input[@placeholder='List name']")
+    @FindBy(css = ".controls>.board-actions>div#stack-add>form>input#new-stack-input-main")
     public WebElement listNameInput;
 
-    @FindBy(xpath = "//div[@id='stack-add']//input[@type='submit']")
+    @FindBy(css = ".controls>.board-actions>div#stack-add>form>input[type='submit']")
     public WebElement listAddArrow;
+
+    @FindBy(xpath = "//div[@class='stack__header']//h3")
+    public List<WebElement> allLists;
+
+    @FindBy(xpath = "//div[@class='stack']//div//button[@icon='icon-add']")
+    public List<WebElement> addCardBtns;
+
+    @FindBy(xpath = "//input[@placeholder='Card name']")
+    public WebElement cardNameInput;
+
+    @FindBy(xpath = "//div[@class='stack__card-add']//input[@type='submit']")
+    public WebElement cardAddArrow;
+
+    @FindBy(xpath = "//div[@class='stack']//h3//span")
+    public List<WebElement> allCards;
+
+
+    public WebElement clickOnAddCardButton(String listName){
+        int count = 0;
+        for (WebElement eachList : allLists) {
+            count++;
+            if (eachList.getText().equals(listName)){
+                break;
+            }
+        }
+        WebElement listNamesAddCardBtn = addCardBtns.get(count-1);
+        return listNamesAddCardBtn;
+    }
+
+    public int selectBoard(String boardName){
+        int count = 0;
+        for (WebElement eachBoard : allBoards) {
+            count++;
+            if (eachBoard.getText().equals(boardName)){
+                break;
+            }
+        }
+        return count-1;
+    }
+
 }
--- src/test/java/com/MeetSky/step_definitions/DeckStepDefs.java
+++ src/test/java/com/MeetSky/step_definitions/DeckStepDefs.java
@@ -38,11 +38,11 @@ public class DeckStepDefs extends BasePage {
     }
     @Given("user types a board name {string}")
     public void user_types_a_board_name(String boardName) {
-        deckPage.boardNameInput.sendKeys("School" + Keys.ENTER);
+        deckPage.boardNameInput.sendKeys(boardName + Keys.ENTER);
     }
     @Then("user's newly created board {string} is displayed under All boards")
     public void user_s_newly_created_board_is_displayed_under_all_boards(String expectedBoardName) {
-        for (WebElement eachBoard : deckPage.boards) {
+        for (WebElement eachBoard : deckPage.allBoards) {
             String actualBoardName = eachBoard.getText();
             if(actualBoardName.equals(expectedBoardName)){
                 Assert.assertEquals("Newly created board verification failed!", actualBoardName, expectedBoardName);
@@ -50,22 +50,62 @@ public class DeckStepDefs extends BasePage {
         }
     }
 
-    @Given("user selects a board")
-    public void user_selects_a_board() {
+    @Given("user selects a {string} board")
+    public void user_selects_a_board(String boardNameToSelect) {
+        //BrowserUtils.waitForVisibilityOf(deckPage.allBoards);
+        int boardIndexToClick = deckPage.selectBoard(boardNameToSelect);
+        deckPage.allBoards.get(boardIndexToClick).click();
 
     }
     @Given("user clicks on Add list button")
     public void user_clicks_on_add_list_button() {
+        BrowserUtils.waitForVisibility(deckPage.addListBtn, 5);
         deckPage.addListBtn.click();
     }
-    @Given("user types a list name and clicks on arrow")
-    public void user_types_a_list_name_and_clicks_on_arrow() {
-        deckPage.listNameInput.sendKeys("Home List");
+
+    @Given("user types a list name {string} and clicks on arrow")
+    public void user_types_a_list_name_and_clicks_on_arrow(String listName) {
+        BrowserUtils.waitForVisibility(deckPage.listNameInput, 5);
+        deckPage.listNameInput.sendKeys(listName);
         deckPage.listAddArrow.click();
     }
-    @Then("user should be able to see the created list")
-    public void user_should_be_able_to_see_the_created_list() {
+    @Then("user should be able to see {string} list under {string} board")
+    public void user_should_be_able_to_see_list_under_board(String expectedListName, String expectedBoardName) {
+        String actualListName = "";
+        for (WebElement eachList : deckPage.allLists) {
+            if (expectedListName.equals(eachList.getText())){
+                actualListName = eachList.getText();
+            }
+        }
 
+        Assert.assertEquals(expectedBoardName, deckPage.selectedBoardName.getText());
+        Assert.assertEquals(expectedListName, actualListName);
     }
+
+    @Given("user clicks on Add card button of {string} list")
+    public void user_clicks_on_add_card_button_of_list(String listName) {
+        deckPage.clickOnAddCardButton(listName).click();
+    }
+
+    @Given("user types a card name {string} and clicks on arrow")
+    public void user_types_a_card_name_and_clicks_on_arrow(String cardName) {
+        deckPage.cardNameInput.sendKeys(cardName);
+        deckPage.cardAddArrow.click();
+    }
+    @Then("user should be able to see {string} card under {string} list")
+    public void user_should_be_able_to_see_card_under_list(String cardName, String listName) {
+        for (WebElement eachList : deckPage.allLists) {
+            if(listName.equals(eachList.getText())){
+                for (WebElement eachCard : deckPage.allCards) {
+                    if(cardName.equals(eachCard.getText())){
+                        Assert.assertTrue(eachCard.isDisplayed());
+                    }
+                }
+            }
+        }
+    }
+
+
+
 
 }
